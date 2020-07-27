f755559ce01aa9621f3c10e6f998f3e8dfd448ac
Дима Мироненко <city7gor@gmail.com>
2020-07-27 23:01:42 +0300

    city7g-dev | Add template advantage | city7g

---
+++ components/Mining/List.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="c-mining-list">
+    <template v-for="item of list">
+      <div class="c-mining-list__item" :key="item.title" style="{ background: item.img }">
+        <div class="container" :class="{ 'container-right': item.aligh }">
+          <h1 class="c-mining-list__title">{{ item.title }}</h1>
+          <h2 class="c-mining-list__sub">{{ item.subTitle }}</h2>
+          <p class="c-mining-list__text">{{ item.text }}</p>
+          <nuxt-link to="/" class="c-mining-list__link blue-btn-arrow">
+            <span>{{ item.btnText }}</span>
+          </nuxt-link>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiningList",
+  props: {
+    list: Array,
+  },
+};
+</script>
+
+<style lang="scss">
+.c-mining-list {
+  &__item {
+    padding: rem(100) rem(15);
+  }
+
+  .container {
+    width: 100%;
+    max-width: rem(1072);
+    margin: 0 auto;
+  }
+
+  .container-right {
+    padding-left: rem(526);
+  }
+
+  &__title {
+    @extend %h1-title;
+    margin-bottom: rem(30);
+    max-width: rem(800);
+  }
+
+  &__sub {
+    @extend %h2-title;
+    margin-bottom: rem(20);
+    max-width: rem(499);
+  }
+
+  &__text {
+    @extend %regular;
+    @include font_sizes(25, 40);
+    margin-bottom: rem(50);
+    max-width: rem(500);
+  }
+}
+</style>
