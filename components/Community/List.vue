9618848071d47bac3fa5244d9bb53d40d8744af6
Дима Мироненко <city7gor@gmail.com>
2020-07-30 11:34:49 +0300

    city7g-dev | Commit | city7g

---
+++ components/Community/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="c-community-list">
+    <div class="container">
+      <h1 class="c-community-list__title">{{ title }}</h1>
+      <p class="c-community-list__text">{{ text }}</p>
+      <div class="c-community-list__item" v-for="item of list" :key="item.title">
+        <h1 class="c-community-list__item-title">{{ item.title }}</h1>
+        <p class="c-community-list__item-text">{{ item.text }}</p>
+        <figure>
+          <img :src="item.img" alt />
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommunityList",
+  props: {
+    title: String,
+    text: String,
+    list: Array,
+  },
+};
+</script>
+
+<style lang="scss">
+.c-community-list {
+  padding: rem(100) rem(15);
+
+  .container {
+    width: 100%;
+    max-width: rem(825);
+    margin: 0 auto;
+  }
+
+  &__title {
+    @extend %h1-title;
+    margin-bottom: rem(50);
+    text-align: center;
+  }
+
+  &__text {
+    @extend %text-main;
+    text-align: center;
+    margin-bottom: rem(20);
+  }
+
+  &__item {
+    padding: rem(30);
+  }
+}
+</style>
---
+++ pages/community.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="v-community">
+    <section class="v-community-section">
+      <BigHeader
+        title="Community"
+        text="Thank you for your interest in the PKT Network. In building out this network infrastructure we are only as strong as the sum of all parts. ."
+        bg="/img/community-header-bg.jpg"
+      />
+    </section>
+    <section class="v-community-section">
+      <CommunityList
+        title="Resources"
+        text="Participate in the PKT Trading Group to find buy and sell sides for PKT liquidity. This community resource is open to the public to initiate block trades of PKT"
+        :list="resources_list"
+      />
+    </section>
+  </div>
+</template>
+
+<script>
+import BigHeader from "~/components/Base/BigHeader";
+import CommunityList from "~/components/Community/List";
+
+export default {
+  components: {
+    BigHeader,
+    CommunityList,
+  },
+  data() {
+    return {
+      resources_list: [
+        {
+          title: "PKT Trading Group",
+          text:
+            "Participate in live discussions, ask questions, and get to know the PKT community.",
+          img: "/img/communicate/telegram.svg",
+        },
+        {
+          title: "PKT Trading Group",
+          text:
+            "Participate in live discussions, ask questions, and get to know the PKT community.",
+          img: "/img/communicate/telegram.svg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.test {
+  @extend %h1-title;
+  height: calc(100vh - 100px);
+  background-color: grey;
+}
+</style>
