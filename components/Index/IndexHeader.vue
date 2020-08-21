e04711aeb2526965531858305cdb77ade317f85f
Bogdan Crius <crius.work@gmail.com>
2020-08-21 21:15:37 +0300

    master | Fix bugs | Bohdan K.

--- components/Index/IndexHeader.vue
+++ components/Index/IndexHeader.vue
@@ -173,19 +173,19 @@ export default {
           order: 1;
         }
         &:nth-child(2) {
-          order: 4;
+          order: 6;
         }
         &:nth-child(3) {
-          order: 3;
-        }
-        &:nth-child(4) {
           order: 2;
         }
-        &:nth-child(5) {
+        &:nth-child(4) {
           order: 5;
         }
+        &:nth-child(5) {
+          order: 3;
+        }
         &:nth-child(6) {
-          order: 6;
+          order: 4;
         }
       }
     }
