60ae4e04d8878a664d1e994d4191317a914b1d8e
Bogdan Crius <crius.work@gmail.com>
2020-08-27 08:30:55 +0300

    master | Fix bugs | Bohdan K.

--- components/Community/Contact.vue
+++ components/Community/Contact.vue
@@ -8,7 +8,7 @@
         <input type="text" placeholder="Subject:" v-model="form.subject" class="c-community-contact__input" />
         <textarea placeholder="Message:" v-model="form.message" class="c-community-contact__textarea"></textarea>
         <figure class="c-community-contact__captcha">
-          <vue-recaptcha sitekey="6LfdA8EZAAAAAElKtBfaI0iB6QoChOR_LJ6MX3UP"></vue-recaptcha>
+          <vue-recaptcha sitekey="6Leho8MZAAAAAH5GlD2YVJWtyen8wxOXYlpbysMn" :loadRecaptchaScript="true"></vue-recaptcha>
         </figure>
         <button type="submit" class="c-community-contact__button blue-btn">Send Message</button>
         <div class="c-community-contact__status">
