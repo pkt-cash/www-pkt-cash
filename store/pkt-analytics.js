1f7f85cbde47e906895408f15929bd7445ff46c0
Bogdan Crius <crius.work@gmail.com>
2020-08-28 17:58:03 +0300

    master | Fix bugs | Bohdan K.

--- store/pkt-analytics.js
+++ store/pkt-analytics.js
@@ -36,6 +36,24 @@ export const actions = {
       const difficulty = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/chain/down/1')
       const stats = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/packetcrypt/stats/1/')
 
+      commit('updateField', { path: 'already_mined', value: data.alreadyMined })
+      commit('updateField', { path: 'reward', value: data.reward })
+      commit('updateField', { path: 'remaining', value: data.remaining })
+      commit('updateField', { path: 'difficulty', value: Math.floor(difficulty.results[0].difficulty) })
+      commit('updateField', { path: 'bitsPerSecond', value: stats.results[0].bitsPerSecond })
+      commit('updateField', { path: 'encryptionsPerSecond', value: stats.results[0].encryptionsPerSecond * 1000000 })
+
+    } catch (e) {
+      console.log(e)
+      // commit('updateField', { path: 'status', value: 'error' })
+    }
+  },
+  async update_data({ commit }) {
+    try {
+      const data = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/stats/coins');
+      // /api/v1/PKT/pkt/chain/down
+      const difficulty = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/chain/down/1')
+      const stats = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/packetcrypt/stats/1/')
       console.log(data, '/stats/coins')
       console.log(difficulty, '/chain/down/1')
       console.log(stats, '/packetcrypt/stats/1/')
@@ -46,19 +64,6 @@ export const actions = {
       commit('updateField', { path: 'difficulty', value: Math.floor(difficulty.results[0].difficulty) })
       commit('updateField', { path: 'bitsPerSecond', value: stats.results[0].bitsPerSecond })
       commit('updateField', { path: 'encryptionsPerSecond', value: stats.results[0].encryptionsPerSecond * 1000000 })
-    } catch (e) {
-      // commit('updateField', { path: 'status', value: 'error' })
-    }
-  },
-  async update_data({ commit }) {
-    try {
-      const data = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/stats/coins');
-      const difficulty = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/chain/down/1')
-
-      commit('updateField', { path: 'already_mined', value: data.alreadyMined })
-      commit('updateField', { path: 'reward', value: data.reward })
-      commit('updateField', { path: 'remaining', value: data.remaining })
-      commit('updateField', { path: 'difficulty', value: Math.floor(difficulty.results[0].difficulty) })
     } catch (e) {
 
     }
