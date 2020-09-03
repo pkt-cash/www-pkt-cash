1531d45065d3af5e09a8b194ce574611abf9a311
Дима Мироненко <city7gor@gmail.com>
2020-09-04 00:20:29 +0300

    master | Fixed retina device | city7g

--- components/Index/IndexHeader.vue
+++ components/Index/IndexHeader.vue
@@ -304,12 +304,12 @@ export default {
 }
 .c-index-header__desk {
   &-list {
-    margin-top: rem(-180);
+    margin-top: rem(-120);
     width: 100%;
     display: flex;
     justify-content: space-between;
     @include for-width(-desktop-lg) {
-      margin-top: rem(-120);
+      margin-top: rem(-90);
     }
     @include for-width(-desktop-medium) {
       margin-top: rem(-50);
