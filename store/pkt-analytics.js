12a631f2add29701e4a96bce819a033a500cbe19
Caleb James DeLisle <cjd@cjdns.fr>
2021-05-17 21:41:04 +0200

    Explorer api is nolonger https://pkt.cash/

--- store/pkt-analytics.js
+++ store/pkt-analytics.js
@@ -1,5 +1,7 @@
-import {Map as immutableMap} from 'immutable';
-import {getField, updateField} from 'vuex-map-fields';
+import { Map as immutableMap } from 'immutable';
+import { getField, updateField } from 'vuex-map-fields';
+
+const EXPLORER_API = 'https://explorer.pkt.cash';
 
 const STATE = immutableMap({
   date: Date.now(),
@@ -31,10 +33,10 @@ export const actions = {
   async load({ commit }) {
     try {
       // { alreadyMined, reward, remaining }
-      const data = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/stats/coins');
+      const data = await this.$axios.$get(`${EXPLORER_API}/api/v1/PKT/pkt/stats/coins`);
       // /api/v1/PKT/pkt/chain/down
-      const difficulty = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/chain/down/1')
-      const stats = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/packetcrypt/stats/1/')
+      const difficulty = await this.$axios.$get(`${EXPLORER_API}/api/v1/PKT/pkt/chain/down/1`)
+      const stats = await this.$axios.$get(`${EXPLORER_API}/api/v1/PKT/pkt/packetcrypt/stats/1/`)
       console.log('load function')
       commit('updateField', { path: 'already_mined', value: data.alreadyMined })
       commit('updateField', { path: 'reward', value: data.reward })
@@ -50,10 +52,10 @@ export const actions = {
   },
   async update_data({ commit }) {
     try {
-      const data = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/stats/coins');
+      const data = await this.$axios.$get(`${EXPLORER_API}/api/v1/PKT/pkt/stats/coins`);
       // /api/v1/PKT/pkt/chain/down
-      const difficulty = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/chain/down/1')
-      const stats = await this.$axios.$get('https://pkt.cash/api/v1/PKT/pkt/packetcrypt/stats/1/')
+      const difficulty = await this.$axios.$get(`${EXPLORER_API}/api/v1/PKT/pkt/chain/down/1`)
+      const stats = await this.$axios.$get(`${EXPLORER_API}/api/v1/PKT/pkt/packetcrypt/stats/1/`)
       console.log(data, '/stats/coins')
       console.log(difficulty, '/chain/down/1')
       console.log(stats, '/packetcrypt/stats/1/')
