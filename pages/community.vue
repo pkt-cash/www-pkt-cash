59d9ca95fe75cf5d25069c83b92bb66217908776
Coconut3 <juliya.hard@gmail.com>
2022-05-14 14:06:51 -0500

    Revert "Revert "Fixed link for the Twitter""
    This reverts commit 6a05f232b160476ae86ac3fe3848017e9f34df61.

--- pages/community.vue
+++ pages/community.vue
@@ -95,7 +95,7 @@ export default {
           title: this.$t("community.join_list_item_title_4"),
           text: this.$t("community.join_list_item_text_4"),
           img: "/img/community/twitter.svg",
-          link: "https://twitter.com/pkt_cash",
+          link: "https://twitter.com/pktcash",
         },
         /*{
           title: "Mastadon",
