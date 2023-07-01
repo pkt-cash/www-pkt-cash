74d94db88f8955e9f56bbb13897776ee11eabad0
coconut3-dev <juliya.hard@gmail.com>
2023-06-30 22:49:22 -0500

    Updated top links to Whitepapers
     - added link to Packetcrypt
     - turned whitepaper to dropdown

--- components/Header.vue
+++ components/Header.vue
@@ -19,11 +19,31 @@
         </div>
         <div :id="$i18n.locale" class="c-navigation__top_links">
           <a href="https://docsend.com/view/ayf5d3tz5rymn8fv" target="_blank" class="c-navigation__top_links__link c-navigation__top_links__link__deck">Deck</a>
-          <a href="/PKT_Network_v1.0_2021.02.01.pdf" target="_blank" class="c-navigation__top_links__link_whitepaper" id="en">{{ $t("header.whitepaper") }}</a>
-          <a href="/PKT_Network_v1.0_2021.02.01-ja.pdf" target="_blank" class="c-navigation__top_links__link_whitepaper" id="ja">{{ $t("header.whitepaper") }}</a>
-          <a href="/PKT_Network_v1.0_2021.02.01-ch.pdf" target="_blank" class="c-navigation__top_links__link_whitepaper" id="zh">{{ $t("header.whitepaper") }}</a>
-          <a href="/PKT_Network_v1.0_2021.02.01-ko.pdf" target="_blank" class="c-navigation__top_links__link_whitepaper" id="ko">{{ $t("header.whitepaper") }}</a>
-          <a href="/PKT_Network_v1.0_2021.02.01-ru.pdf" target="_blank" class="c-navigation__top_links__link_whitepaper" id="ru">{{ $t("header.whitepaper") }}</a>
+          
+          <div class="nav__menu-top_menu">
+            <ul class="nav__menu-list">
+              <li class="nav__menu-item">
+                  <a class="nav__menu-link c-navigation__top_links__link">
+                    {{ $t("header.whitepaper") }}
+                    <svg class="" width="10" height="9" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 284.929 284.929" style="enable-background: new 0 0 284.929 284.929" xml:space="preserve" fill="#fff">
+                      <g><path d="M282.082,76.511l-14.274-14.273c-1.902-1.906-4.093-2.856-6.57-2.856c-2.471,0-4.661,0.95-6.563,2.856L142.466,174.441
+                      L30.262,62.241c-1.903-1.906-4.093-2.856-6.567-2.856c-2.475,0-4.665,0.95-6.567,2.856L2.856,76.515C0.95,78.417,0,80.607,0,83.082
+                      c0,2.473,0.953,4.663,2.856,6.565l133.043,133.046c1.902,1.903,4.093,2.854,6.567,2.854s4.661-0.951,6.562-2.854L282.082,89.647
+                      c1.902-1.903,2.847-4.093,2.847-6.565C284.929,80.607,283.984,78.417,282.082,76.511z" /> 
+                      </g>
+                    </svg>
+                  </a>
+                  <ul class="nested-drop">
+                    <li class="locale" id="en"><a href="/PKT_Network_v1.0_2021.02.01.pdf" target="_blank" class="nav__menu-link-child">PKT Network</a></li>
+                    <li class="locale" id="ja"><a href="/PKT_Network_v1.0_2021.02.01-ja.pdf" target="_blank" class="nav__menu-link-child">PKT Network</a></li>
+                    <li class="locale" id="zh"><a href="/PKT_Network_v1.0_2021.02.01-ch.pdf" target="_blank" class="nav__menu-link-child">PKT Network</a></li>
+                    <li class="locale" id="ko"><a href="/PKT_Network_v1.0_2021.02.01-ko.pdf" target="_blank" class="nav__menu-link-child">PKT Network</a></li>
+                    <li class="locale" id="ru"><a href="/PKT_Network_v1.0_2021.02.01-ru.pdf" target="_blank" class="nav__menu-link-child">PKT Network</a></li>
+                    <li><a href="/PacketCrypt-2020-09-04.pdf" target="_blank" class="nav__menu-link-child">PacketCrypt</a></li>
+                  </ul>
+              </li>
+            </ul>
+          </div>
           <a href="https://docs.pkt.cash" target="_blank" class="c-navigation__top_links__link">Docs</a>
           <div class="c-navigation__top_links_social" v-for="item of social_list">
             <template>
@@ -765,6 +785,49 @@ export default {
         padding:0 rem(4);
         & img {
           vertical-align: middle;
+        }
+      }
+    }
+    & .nav__menu-top_menu {
+      @include for-width(-small-lg) {
+        margin-right:rem(20);
+      }
+      & .nav__menu-list {
+        @include for-width(-tablet) {    
+          transform: scale(1);
+          position: relative;
+        }
+        & .nav__menu-item {
+          margin:0;
+          & .nested-drop {
+            margin-left: -10px;
+            z-index: 2;
+            top:90%;
+            background-color: #222450;
+            width:100%;
+            @include for-width(-tablet) {
+              position: absolute;
+            }
+            @include for-width(-small-lg) {
+              width:120%;
+            }
+          }
+          & li {
+            padding:rem(15) rem(12) 0;
+            &.locale {
+              display:none;
+            }
+            & .nav__menu-link-child {
+              @extend %light;
+              color: $white;
+              &:hover {
+                color: $blue_new;
+              }
+              @include for-width(-laptop) {
+                font-size: rem(14);
+              }
+            }
+          }
         }
       }
     }
